6af47d73a75b6f9932b6bf4ae2850dbbdf98161f
Geovic Fagel <geo@Geovics-MacBook-Pro.local>
2019-07-11 09:14:44 +0800

    adding components

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+// node.js
+
+import OverVue from './OverVue'
+import Carousel from './Carousel'
+import SideBar from './Sidebar'
+// importAdditions
+
+let Exports = {
+  overVue: OverVue,
+  sideBar: SideBar,
+  carousel: Carousel,
+// exportAdditions
+
+}
+export default Exports
